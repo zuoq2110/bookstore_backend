6689c26d0d17d4cd668d659f36b31e69e62d1113
unknown <zuoq2110@gmail.com>
2023-11-24 16:45:46 +0700

    Tao du an moi

---
+++ src/main/java/com/example/web_ban_sach/WebBanSachApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_ban_sach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanSachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_ban_sach/WebBanSachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_ban_sach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanSachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
